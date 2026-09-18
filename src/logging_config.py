8775fbd969479e38dc2f41a17f035968c92c28cc
nhesen <hnurmemmedov12@gmail.com>
2026-09-19 01:54:45 +0400

    fix env log level

--- src/logging_config.py
+++ src/logging_config.py
@@ -2,9 +2,12 @@ import logging
 import os
 
 LOG_FORMAT = "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
-LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+# LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")  #fayl import olunanda bir dəfə oxunur.Pytest-də environment test zamanı dəyişsə, bu dəyər köhnə qala bilər.
 
-def setup_logging(log_level=LOG_LEVEL):
+# def setup_logging(log_level=LOG_LEVEL):
+def setup_logging(log_level=None):
+    if log_level is None:
+        log_level = os.getenv("LOG_LEVEL", "INFO")
     logging.basicConfig(
         level=log_level.upper(),
         format=LOG_FORMAT,
